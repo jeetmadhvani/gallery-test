ef1708029c0f2e4b31757eb255cd0d7f2f31129c
Jeet Madhvani <madhavanijeet123@gmail.com>
2026-01-14 22:40:51 +0530

    secondd committ

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+createRoot(document.getElementById('root')).render(
     <App />
-  </HashRouter>
 )
